1a555527d69f4ae5f0724e54b72d9da71f708f36
unknown <vdpham0@gmail.com>
2022-09-30 01:21:41 +0200

    python-basics-done

---
+++ 9_2-standard_library.py
@@ -0,0 +1,18 @@
+import random
+courses = ["History", "Math", "Physics", "CompSci"]
+random_course = random.choice(courses)
+print(random_course)
+
+import math
+
+rads = math.radians(90)
+
+import datetime
+
+today = datetime.date.today()
+print(today)
+
+import os
+
+print(os.getcwd())
+print(os.__file__)
